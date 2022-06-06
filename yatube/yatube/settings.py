9682731afe4207d17f2f9d871d323a1bda0b7340
leoneed <leoneed@mbp-leoneed.local>
2022-06-06 23:23:59 +0500

    some fixes

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'g6u=&r@lqls-4h=a8bp@ibe%4v63sx%#gpy%fi4zrcoh19xdrk'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
